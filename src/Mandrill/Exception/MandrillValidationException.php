776a79bc9aed56252facb3111a38bf687cfe592a
Dan McAdams <danmcadams@gmail.com>
2018-09-03 14:06:58 -0500

    add validation exception

--- src/Mandrill/Exception/MandrillValidationException.php
+++ src/Mandrill/Exception/MandrillValidationException.php
@@ -11,7 +11,12 @@
 
 namespace DZMC\Mandrill\Exception;
 
-
+/**
+ * Class MandrillValidationException
+ *
+ * @author  Dan McAdams
+ * @package DZMC\Mandrill\Exception
+ */
 class MandrillValidationException extends \Exception
 {
 }
