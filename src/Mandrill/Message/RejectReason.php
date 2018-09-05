dffaf6be8d76169a73d36ea15cae5affe4d085cd
Dan McAdams <danmcadams@gmail.com>
2018-09-04 19:10:42 -0500

    add rejection reasons

---
+++ src/Mandrill/Message/RejectReason.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of the danmcadams/mandrill-sdk-wrapper package.
+ *
+ * (c) Dan McAdams <danmcadams@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace DZMC\Mandrill\Message;
+
+
+/**
+ * Class RejectReason
+ *
+ * The reason for the rejection if the recipient status is "rejected"
+ *
+ * @author  Dan McAdams
+ * @package DZMC\Mandrill\Message
+ */
+final class RejectReason
+{
+    const HARD_BOUNCE     = 'hard-bounce';
+    const SOFT_BOUNCE     = 'soft-bounce';
+    const SPAM            = 'spam';
+    const UNSUB           = 'ubsub';
+    const CUSTOM          = 'custom';
+    const INVALID_SENDER  = 'invalid-sender';
+    const INVALID         = 'invalid';
+    const TEST_MODE_LIMIT = 'test-mode-limit';
+    const UNSIGNED        = 'unsigned';
+    const RULE            = 'rule';
+}
