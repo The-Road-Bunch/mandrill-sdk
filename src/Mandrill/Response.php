c0ec142aafef313a7fe38d40e8991d259dd7e813
Dan McAdams <danmcadams@gmail.com>
2018-09-06 17:56:08 -0500

    add generic response class.

---
+++ src/Mandrill/Response.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of the danmcadams/mandrill-sdk-wrapper package.
+ *
+ * (c) Dan McAdams <danmcadams@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace DZMC\Mandrill;
+
+
+/**
+ * Class Response
+ *
+ * @author  Dan McAdams
+ * @package DZMC\Mandrill
+ */
+class Response
+{
+    /**
+     * The response provided from the mandrill sdk is an array
+     * we'll just store it in here for now
+     *
+     * @var array $response
+     */
+    protected $response;
+
+    /**
+     * Response constructor.
+     *
+     * @param array $mandrillResponse
+     */
+    public function __construct(array $mandrillResponse)
+    {
+        $this->response = $mandrillResponse;
+    }
+
+    /**
+     * @param string $index
+     *
+     * @return mixed
+     */
+    public function get(string $index)
+    {
+        if (!empty($this->response[$index])) {
+            return $this->response[$index];
+        }
+        return null;
+    }
+
+    /**
+     * Return the raw array provided by mandrill
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->response;
+    }
+}
---
+++ tests/ResponseTest.php
@@ -0,0 +1,75 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of the danmcadams/mandrill-sdk-wrapper package.
+ *
+ * (c) Dan McAdams <danmcadams@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace DZMC\Mandrill\Tests;
+
+use DZMC\Mandrill\Message\RejectReason;
+use DZMC\Mandrill\Response;
+use PHPUnit\Framework\TestCase;
+
+
+/**
+ * Class ResponseTest
+ *
+ * @author  Dan McAdams
+ * @package DZMC\Mandrill\Tests
+ */
+class ResponseTest extends TestCase
+{
+    public function testCreateResponse()
+    {
+        $response = new Response([]);
+        $this->assertNotNull($response);
+    }
+
+    public function testCreateResponseWithArray()
+    {
+        $response = new Response([]);
+        $this->assertEquals([], $response->toArray());
+    }
+
+    public function testGetResponseByIndex()
+    {
+        $mandrillResponse = [
+            'email' => 'test@example.com'
+        ];
+
+        $response = new Response($mandrillResponse);
+
+        $this->assertNotNull($response->get('email'));
+        $this->assertEquals($mandrillResponse['email'], $response->get('email'));
+    }
+
+    public function testGetResponseIndexNotFound()
+    {
+        // create an empty response
+        $response = new Response([]);
+        $this->assertNull($response->get('email'));
+    }
+
+    public function testFullResponseArray()
+    {
+        $mandrillResponse = [
+            'email'         => 'test@example.com',
+            'status'        => 'sent',
+            'reject-reason' => RejectReason::SPAM,
+            '_id'           => uniqid()
+        ];
+
+        $response = new Response($mandrillResponse);
+
+        $this->assertEquals($mandrillResponse, $response->toArray());
+
+        foreach ($mandrillResponse as $key => $item) {
+            $this->assertEquals($item, $response->get($key));
+        }
+    }
+}
