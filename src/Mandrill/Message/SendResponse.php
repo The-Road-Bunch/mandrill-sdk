e1ca777c81e4054d253b455edd77a13ca249aecd
Dan McAdams <danmcadams@gmail.com>
2018-09-08 14:09:30 -0500

    add SendResponse, this will be the same response that is provided when sending a template message as well

---
+++ src/Mandrill/Message/SendResponse.php
@@ -0,0 +1,89 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of the danmcadams/mandrill-sdk-wrapper package.
+ *
+ * (c) Dan McAdams <danmcadams@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace DZMC\Mandrill\Message;
+
+
+/**
+ * Class SendResponse
+ *
+ * @author  Dan McAdams
+ * @package DZMC\Mandrill\Message
+ */
+class SendResponse
+{
+    /**
+     * the message's unique id
+     *
+     * @var string $id
+     */
+    public $id;
+
+    /**
+     * the email address of the recipient
+     *
+     * @var string $email
+     */
+    public $email;
+
+    /**
+     * the sending status of the recipient
+     *      - either "sent", "queued", "scheduled", "rejected", or "invalid"
+     *
+     * @var string $status
+     */
+    public $status;
+
+    /**
+     * the reason for the rejection if the recipient status is "rejected"
+     *      - one of "hard-bounce", "soft-bounce", "spam", "unsub", "custom",
+     *        "invalid-sender", "invalid", "test-mode-limit", "unsigned", or "rule"
+     *
+     * @var string $rejectReason
+     */
+    public $rejectReason;
+
+    /**
+     * SendResponse constructor.
+     * @param string      $id
+     * @param string      $email
+     * @param string      $status
+     * @param string|null $rejectReason
+     */
+    public function __construct(string $id, string $email, string $status, string $rejectReason = null)
+    {
+        $this->id           = $id;
+        $this->email        = $email;
+        $this->status       = $status;
+        $this->rejectReason = $rejectReason;
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return [
+            'email'         => $this->email,
+            'status'        => $this->status,
+            'reject_reason' => $this->rejectReason,
+            '_id'           => $this->id
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return json_encode($this->toArray());
+    }
+}
---
+++ tests/Message/SendResponseTest.php
@@ -0,0 +1,72 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of the danmcadams/mandrill-sdk-wrapper package.
+ *
+ * (c) Dan McAdams <danmcadams@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace DZMC\Mandrill\Tests\Message;
+
+
+use DZMC\Mandrill\Message\SendResponse;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class SendResponseTest
+ *
+ * @author  Dan McAdams
+ * @package DZMC\Mandrill\Tests\Message
+ */
+class SendResponseTest extends TestCase
+{
+    public function testCreateSendResponse()
+    {
+        $mr = $this->getSampleMandrillResponse();
+        $sr = $this->createSendResponse($mr);
+
+        $this->assertEquals($mr['_id'], $sr->id);
+        $this->assertEquals($mr['email'], $sr->email);
+        $this->assertEquals($mr['status'], $sr->status);
+        $this->assertEquals($mr['reject_reason'], $sr->rejectReason);
+    }
+
+    public function testSendResponseToArray()
+    {
+        $mr = $this->getSampleMandrillResponse();
+        $sr = $this->createSendResponse($mr);
+
+        $this->assertEquals($mr, $sr->toArray());
+    }
+
+    public function testToStringReturnProperJson()
+    {
+        $mr = $this->getSampleMandrillResponse();
+        $sr = $this->createSendResponse($mr);
+
+        $this->assertEquals(json_encode($mr), (string) $sr);
+    }
+
+    private function getSampleMandrillResponse(): array
+    {
+        return [
+            'email'         => 'test@example.com',
+            'status'        => 'sent',
+            'reject_reason' => null,
+            '_id'           => uniqid()
+        ];
+    }
+
+    /**
+     * @param array $mr A response from Mandrill
+     * @return SendResponse
+     */
+    private function createSendResponse(array $mr): SendResponse
+    {
+        $sr = new SendResponse($mr['_id'], $mr['email'], $mr['status'], $mr['reject_reason']);
+        return $sr;
+    }
+}
